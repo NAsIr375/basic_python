63ac10a2145fbb4af6cd70b55513164dc09bda4a
Md Nasir Uddin <124335344+NAsIr375@users.noreply.github.com>
2024-03-22 17:01:44 +0530

    Add files via upload

---
+++ logical_and_operator.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+if n>=0 and n%2 == 0:
+    print(n,"is positive and even number")
+elif n>=0 and n%2 !=0:
+    print(n,"is positive number and odd number")
+elif n<0 and n%2 ==0:
+    print(n,"is nagetive number and even number")
+else:
+     print(n,"is nagetive number and odd number")
